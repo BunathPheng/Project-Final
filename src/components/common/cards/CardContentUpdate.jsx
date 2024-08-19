447470903fbf081d98dd891b7b7a5cac4a706b2b
Sokreaksmey Deap <sokreaksmey1827@gmail.com>
2024-08-19 12:07:52 +0700

    update:updated navbar

--- src/components/common/cards/CardContentUpdate.jsx
+++ src/components/common/cards/CardContentUpdate.jsx
@@ -5,16 +5,16 @@ export default function CardContentUpdate({ img, title, des, value }) {
   return (
     <>
       <a href={value}>
-        <div className="bg-white  rounded-tl-[10%] rounded-br-[10%] border-gray-100 border-1 ">
+        <div className="bg-slate-100  rounded-tl-[10%] rounded-br-[10%] border-gray-100 border-1 pb-8">
           <img
             className="w-[500px] h-[350px] img-new object-cover rounded-tl-[10%] "
             src={img}
             alt={title}
           />
-          <h1 className=" font-new text-[24px] font-bold text-second text-center mt-5 line-clamp-1">
+          <h1 className=" font-new text-[24px] font-bold text-second text-center mt-5 line-clamp-1 px-5">
             {title}
           </h1>
-          <p className="font-suwannaphum  text-[16px] text-center pt-1 pb-6 px-4 line-clamp-3">
+          <p className="font-suwannaphum  text-[16px] text-center pb-6 px-4 line-clamp-3 pt-2">
             {des}
           </p>
         </div>

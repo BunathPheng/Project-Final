d62ae819da78b28eb82e72f1849b592ff3027210
Sokreaksmey Deap <sokreaksmey1827@gmail.com>
2024-08-19 21:04:52 +0700

    commit file

--- src/components/common/ContenSkillLevel.jsx
+++ src/components/common/ContenSkillLevel.jsx
@@ -11,9 +11,9 @@ export default function ContenSkillLevel({ skill_name, description, img }) {
         />
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       </div>
-      <div className="absolute bottom-28 md:left-14 left-10">
+      <div className="absolute bottom-28 md:left-[40px] px-[20px] md:px-[5px] lg:px-[7px]">
         <h1 className="text-[32px] font-bold text-second">{skill_name}</h1>
-        <p className="text-white mt-2 text-xl">{description}</p>
+        <p className="text-white mt-2 text-xl text-start">{description}</p>
       </div>
     </>
   );

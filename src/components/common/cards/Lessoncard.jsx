9d4e724938b9393d97f277c57f6a6c4f870acc6c
pheng bunath <bunathpheng4@gmail.com>
2024-08-19 20:07:22 +0700

    update_code

--- src/components/common/cards/Lessoncard.jsx
+++ src/components/common/cards/Lessoncard.jsx
@@ -10,24 +10,22 @@ export default function Lessoncard({ onClick, title, des, pic, uuid }) {
   };
 
   return (
-    <div onClick={handleClick}>
-      <Link to={`${convert}`}>
-        <div className="max-w-[430px] max-h-[261px] rounded-tr-3xl rounded-bl-3xl relative group">
-          <img
-            className="rounded-tr-3xl rounded-bl-3xl brightness-50 w-[400px] h-[250px] object-cover transition-transform duration-300 group-hover:scale-105 group-hover:brightness-75"
-            src={pic}
-            alt="champion"
-          />
-          <div className="absolute inset-0 flex flex-col items-center justify-center text-white transition-colors duration-300">
-            <h1 className="text-[18px] md:text-[20px] lg:text-[24px] font-bold text-center mt-5 drop-shadow-lg shadow-black line-clamp-1 px-5">
-              {title}
-            </h1>
-            <p className="text-[12px] md:text-[18px] text-center mt-1 line-clamp-2 px-5">
-              {des}
-            </p>
-          </div>
+    <Link to={`${convert}`}>
+      <div className="max-w-[430px] max-h-[261px] rounded-tr-3xl rounded-bl-3xl relative group">
+        <img
+          className="rounded-tr-3xl rounded-bl-3xl brightness-50 w-[400px] md:h-[250px] object-cover transition-transform duration-300 group-hover:scale-105 group-hover:brightness-75"
+          src={pic}
+          alt="champion"
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-white transition-colors duration-300">
+          <h1 className="text-[18px] md:text-[20px] lg:text-[24px] font-bold text-center mt-5 drop-shadow-lg shadow-black line-clamp-1 px-5">
+            {title}
+          </h1>
+          <p className="text-[12px] md:text-[18px] text-center mt-1 line-clamp-2 px-5">
+            {des}
+          </p>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
